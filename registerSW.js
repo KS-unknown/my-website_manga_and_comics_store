331150ff6a82a7a6f0a453592b2e92450f1f805e
G2974 <66113041@dpu.ac.th>
2026-04-26 12:31:34 +0700

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/my-website_manga_and_comics_store/sw.js', { scope: '/my-website_manga_and_comics_store/' })})}
